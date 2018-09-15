0d62f61b3a3bd512fa3819c47dca66dc85711a34
Geoff Langenderfer <geoff.langenderfer@gmail.com>
2018-09-14 22:27:20 -0500

    added quit option and logging

--- aprendeSub.py
+++ aprendeSub.py
@@ -1,14 +1,24 @@
+#!/home/geoff/miniconda3/bin/python
 from moviepy.editor import VideoFileClip
 from moviepy.tools import cvsecs
-import re, pygame, time
+import pygame
+
+import re, time, sys, logging
+
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+import smtplib, os
+from email.message import EmailMessage
+import datetime
 
 def file_to_subtitles(filename):
-    """ Converts a srt file into subtitles.
+    """ convierte un .srt archivo en subtitulos
 
-    The returned list is of the form ``[((ta,tb),'some text'),...]``
-    and can be fed to SubtitlesClip.
+    la lista devuelta es de la forma ``[((ta,tb),'some text'),...]``
+    y puede alimentar a SubtitleClip
 
-    Only works for '.srt' format for the moment.
+    solo funciona para el formato .srt por el momento
     """
 
     with open(filename,'r') as f:
@@ -28,35 +38,31 @@ def file_to_subtitles(filename):
             current_text = current_text + line
     return times_texts
 
-def cleanSubs(es, eng):
-    """get rid of non-dialogue subs"""
-    #get rid of all cap english subtitles
+def cleanSubs(s):
+    """eliminar subtitulos sin dialogo"""
     ex=[]
-    for i,v in enumerate(eng):
+    paren = re.compile('\\([a-zA-z\\s]+\\)') #find text in parenthss
+    for i,v in enumerate(s):
+        m = paren.match(v[1])
+        #get rid of all cap subs
         if v[1].isupper():
             ex.append(i)     
+        #whole sub is in (), which are usually sound/noise subs
+        elif  m and len(m.group()) == len(v[1]):
+            ex.append(i)
+        #empty cells
+        elif len(v[1]) == 0:
+            ex.append(i)
+    s = [x for i,x in enumerate(s) if i not in ex]
+    return s 
 
-    eng = [x for i,x in enumerate(eng) if i not in ex]
-
-    #get rid of sound/noise spanish subtitles
-
-    paren = re.compile('\\([a-zA-z\\s]+\\)') #find text in parentheses
-    rem=[]
-    for i,v in enumerate(es):
-        m = paren.match(v[1])
-        #if len(match) is same len(string), then it's a noise/sound sub
-        if  m and len(m.group()) == len(v[1]):
-            rem.append(i)
-
-    es = [x for i,x in enumerate(es) if i not in rem]
-    return es, eng 
-
-def sync(esSubs, engSubs):
-    """sync timestamps of both translations""" 
+def sync(s1, s2):
+    """coordinar timestamps para ambas traducciones""" 
+    #the order of s1, s2 affects the returned value. Why?
 
     #find the file with least entries
-    less  = min( (len(engSubs), engSubs), (len(esSubs), esSubs), key = lambda x: x[0] )[1]
-    more = max( (len(engSubs), engSubs), (len(esSubs), esSubs), key = lambda x: x[0] )[1]
+    less  = min( (len(s2), s2), (len(s1), s1), key = lambda x: x[0] )[1]
+    more = max( (len(s2), s2), (len(s1), s1), key = lambda x: x[0] )[1]
 
     # x is abso% from y 
     abso = lambda x,y: abs( (x-y)/y )
@@ -79,7 +85,6 @@ def sync(esSubs, engSubs):
             elif abs( (xB-yB) ) < .5 and abs( (xE-yE) ) > .5:    
                 z = i
                 while  abs( (xE-yE) ) > .5:
-                    #if z ==70: import pdb; pdb.set_trace()
                     y = more[z]; yE = y[0][1]
                     z+=1
                 entry = ( x[0],  x[1],  "\n".join(x[1] for x in more[i:z+1]) )
@@ -112,18 +117,115 @@ def toFile(s, dest = None):
                 f.write( "{}\n\n".format(item[1]) )
 
     else:
-        print("Don't recognize structure of {}".format(f))
-    
+        print("No reconoce la forma de {}".format(f))
+
+def syncAccuracy(s1, s2, m):
+    """comprobar la calidad de la sincronizacion de subtitulos"""
+    pass
+
+def sendLogs():
+
+    #you need to turn on access for less secure apps to do this. 
+    #https://myaccount.google.com/lesssecureapps
+                
+
+    #pull mail client login info or create file
+    try:
+        with open('credentials.txt') as f:
+            email, username, password = f.readlines()
+           
+        email= email.strip('\n')
+        username = username.strip('\n')
+        password = password.strip('\n')
+            
+    except:
+        print("leer desde credentials.txt no funciono, entonces vamos a hacerlo de nuevo ", sys.exc_info()[0])
+        print("\nTu correo electronico es usado para enviar archivos de registro, que incluye: tiempo para terminar un clip, traducciones, y respuestas ")
+        email = raw_input("Que es tu correo electronico? ")
+        username = raw_input("Que es tu usuario? ")
+        password = raw_input("Que es tu contrasena? ")
+
+        with open('credentials.txt', 'w') as f:
+            f.write(email+'\n')
+            f.write(username+'\n')
+            f.write(password+'\n')
+
+    #determine mail server
+    gmailRe= re.compile('@gmail.com')
+    outlookRe = re.compile('@outlook.com')
+
+    if gmailRe.search(email): SERVER, PORT = 'smtp.gmail.com',587
+    elif outlookRe.search(email): SERVER, PORT = 'smtp-mail.outlook.com',587 
+    else:
+        print("No reconoce tu correo electronico. Envia la mensaje de error y la data.log archivo a geoffro2888@gmail.com para ayuda")
+
+    USER = username
+    PW = password 
+    FROM = email
+    TO = 'geoffro2888@gmail.com'
+
+    #grab log message from file and wipe 
+    path = os.getcwd() + '/data.log'
+    with open(path, 'r') as f:
+        msg = EmailMessage()
+        msg.set_content(f.read())
+    msg['Subject'] = "captionsLearn: User-{} : Date-{}".format( os.getcwd(), datetime.datetime.today().strftime('%Y-%m-%d') )
+    msg['From'] = FROM 
+    msg['To'] = TO 
+    open(path, 'w').close()
+
+
+
+    mailServer = smtplib.SMTP(SERVER, PORT)
+    mailServer.ehlo()
+    mailServer.starttls()
+
+    #email login
+    try:
+        mailServer.login(USER, PW)
+    except SMTPAuthenticationError:
+        print("Intenta ir a https://myaccount.google.com/lesssecureapps y convirtiendo 'less secure apps' a 'on'. Esto permite la programa para enviar correos electronicos desde tu cuenta de gmail.")
+        print("Envia un mensaje a geoffro2888@gmail.com si lo no funciona")
+        raise
+    mailServer.send_message(msg)
+    mailServer.quit()
+    print("correo enviado")
+
 if __name__ == "__main__":
-    video = VideoFileClip(/path/to/video)
-    sub1=file_to_subtitles(/path/to/sub1) 
-    sub2= file_to_subtitles(/path/to/sub2)
-    sub1, sub2= cleanSubs(sub1, sub2)
+    print(30*'-','\n')
+    #print("Bienvenido a aprendeSub. Se te mostrara clips con dialogo, despues te pedira a dar traducciones en ambos linguajes")
+    #Tk().withdraw()
+    #print("Que archivo de video quieres usar?")
+    #vidFile = askopenfilename()
+    #print("Que es el primero archivo de subtitulo para usar?")
+    #sub1File = askopenfilename()
+    #print("Que es el segundo archivo de subtitulos para usar?")
+    #sub2File = askopenfilename()
+    vidFile = '/home/geoff/captionsLearn/lcdp/S01/La.Casa.de.Papel.S01E04.720p.NF.WEB-DL.x265-HETeam.mkv'
+    sub2File = '/home/geoff/captionsLearn/engLcdpSubtitles/Money.Heist.S01E04.XviD-AFG.srt'
+    sub1File = '/home/geoff/captionsLearn/esLcdpSubtitles/La.casa.de.papel.S01E04.WEBRip.Netflix.srt'
+    video = VideoFileClip(vidFile)
+    sub1, sub2 =cleanSubs( file_to_subtitles(sub1File) ), cleanSubs( file_to_subtitles(sub2File) )
     master = sync(sub1, sub2)
+    
+    #setup logging
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s:%(filename)s:%(message)s')
+    file_handler = logging.FileHandler('data.log') 
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    #logging.basicConfig(filename = 'data.log', level = logging.DEBUG, format = '%(asctime)s:%(levelname)s:%(message)s')
+    #initialize data.log
+    logger.info(30*'-'+'(start session)'+30*'-')
+    logger.info("version:{}".format(os.getcwd()))
+    logger.info("video file:{}".format(vidFile))
+    logger.info("sub1 file:{}".format(sub1File))
+    logger.info("sub2 file:{}\n".format(sub2File))
 
     #user sees clips with subtitles
-    print(30*'-','\n')
     for _ in range(len(master)): 
+        start = time.time()
         a,b = master[_][0]
         engSub = master[_][1]
         esSub = master[_][2]
@@ -132,26 +234,52 @@ if __name__ == "__main__":
         clip.preview()
         pygame.quit()
 
-        #boundary(UI)
         #want to replay clip?
         check = ''
-        print("\nTiempo: [{0},{1}]".format(a,b),'\n')
+        i=1
+        print("\nMarca de tiempo: [{0},{1}]".format(a,b),'\n')
         while check not in ['si','s']:
-            replay = input('Replica la clip? ').lower()
+            replay = input('Replica la Clip? ').lower()
             if replay in ['si','s']:
+                i+=1
                 clip.preview()
                 pygame.quit()
             else:
                 check = 'si' 
         #user inputs translations
-        esT = input("Que Uds. dijeron en espanol? ")
-        engT = input("Que Uds. dijeron en ingles? ")
+        esT = input("Que ellos dijeron en espanol? ")
+        enT = input("Que ellos dijeron en ingles? ")
+        
+        #time spent on each clip
+        end = time.time() - start
+
+        logger.info("clip Timestamp:[{0},{1}]".format(a,b))
+        logger.info("Time spent on clip:{0:.2f}s".format(end))
+        logger.info("replay clip {} times".format(i))
+        logger.info("spanish translation:{} ".format(esT))
+        logger.info("spanish translation (ans):{} ".format(esSub))
+        logger.info("english translation:{} ".format(enT))
+        logger.info("english translation (ans):{}\n ".format(engSub))
+        
         print("\nTraduccion Espanol:\n\n", esSub,'\n')
-        print("\Traduccion Ingles:\n\n", engSub,'\n')
+        print("\nTraduccion Ingles:\n\n", engSub,'\n')
+        
+        check3=''
+        while check3 not in ['si','s']:
+            wait= input('Continua aprendiendo? ').lower()
+            if wait in ['si','s','']:
+                break
+            else:
+                print("Listo, regresa pronto!")
+                sendLogs()
+                sys.exit(0)
+        #check whether user wants to review answer before moving on
+        #check2=''
+        #while check2 not in ['yes','y']:
+        #    wait= input('Continue? ').lower()
+        #    if wait in ['yes','y']:
+        #        check2 = 'yes' 
+        #    else:
+        #        time.sleep(4)
+
         print(30*'-','\n')
-        #time.sleep(4)
-
-#log accuracy
-#give a save/quit option
-#remove css <> tags
-
--- captionsLearn.py
+++ captionsLearn.py
@@ -1,6 +1,16 @@
+#!/home/geoff/miniconda3/bin/python
 from moviepy.editor import VideoFileClip
 from moviepy.tools import cvsecs
-import re, pygame, time
+import pygame
+
+import re, time, sys, logging
+
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+import smtplib, os
+from email.message import EmailMessage
+import datetime
 
 def file_to_subtitles(filename):
     """ Converts a srt file into subtitles.
@@ -28,35 +38,31 @@ def file_to_subtitles(filename):
             current_text = current_text + line
     return times_texts
 
-def cleanSubs(es, eng):
+def cleanSubs(s):
     """get rid of non-dialogue subs"""
-    #get rid of all cap english subtitles
     ex=[]
-    for i,v in enumerate(eng):
+    paren = re.compile('\\([a-zA-z\\s]+\\)') #find text in parenthss
+    for i,v in enumerate(s):
+        m = paren.match(v[1])
+        #get rid of all cap subs
         if v[1].isupper():
             ex.append(i)     
+        #whole sub is in (), which are usually sound/noise subs
+        elif  m and len(m.group()) == len(v[1]):
+            ex.append(i)
+        #empty cells
+        elif len(v[1]) == 0:
+            ex.append(i)
+    s = [x for i,x in enumerate(s) if i not in ex]
+    return s 
 
-    eng = [x for i,x in enumerate(eng) if i not in ex]
-
-    #get rid of sound/noise spanish subtitles
-
-    paren = re.compile('\\([a-zA-z\\s]+\\)') #find text in parentheses
-    rem=[]
-    for i,v in enumerate(es):
-        m = paren.match(v[1])
-        #if len(match) is same len(string), then it's a noise/sound sub
-        if  m and len(m.group()) == len(v[1]):
-            rem.append(i)
-
-    es = [x for i,x in enumerate(es) if i not in rem]
-    return es, eng 
-
-def sync(esSubs, engSubs):
+def sync(s1, s2):
     """sync timestamps of both translations""" 
+    #the order of s1, s2 affects the returned value. Why?
 
     #find the file with least entries
-    less  = min( (len(engSubs), engSubs), (len(esSubs), esSubs), key = lambda x: x[0] )[1]
-    more = max( (len(engSubs), engSubs), (len(esSubs), esSubs), key = lambda x: x[0] )[1]
+    less  = min( (len(s2), s2), (len(s1), s1), key = lambda x: x[0] )[1]
+    more = max( (len(s2), s2), (len(s1), s1), key = lambda x: x[0] )[1]
 
     # x is abso% from y 
     abso = lambda x,y: abs( (x-y)/y )
@@ -79,7 +85,6 @@ def sync(esSubs, engSubs):
             elif abs( (xB-yB) ) < .5 and abs( (xE-yE) ) > .5:    
                 z = i
                 while  abs( (xE-yE) ) > .5:
-                    #if z ==70: import pdb; pdb.set_trace()
                     y = more[z]; yE = y[0][1]
                     z+=1
                 entry = ( x[0],  x[1],  "\n".join(x[1] for x in more[i:z+1]) )
@@ -113,17 +118,113 @@ def toFile(s, dest = None):
 
     else:
         print("Don't recognize structure of {}".format(f))
-    
+
+def syncAccuracy(s1, s2, m):
+    """check the quality of the subtitle sync"""
+    pass
+
+def sendLogs():
+
+    #you need to turn on access for less secure apps to do this. 
+    #https://myaccount.google.com/lesssecureapps
+                
+
+    #pull mail client login info or create file
+    try:
+        with open('credentials.txt') as f:
+            email, username, password = f.readlines()
+           
+        email= email.strip('\n')
+        username = username.strip('\n')
+        password = password.strip('\n')
+            
+    except:
+        print("reading from credentials.txt didn't work, so we will create it from scratch", sys.exc_info()[0])
+        print("\nYour email is used to send log files, which include: time to complete a clip, translations, and answers")
+        email = raw_input("whats your email address? ")
+        username = raw_input("what's your username? ")
+        password = raw_input("what's your password? ")
+
+        with open('credentials.txt', 'w') as f:
+            f.write(email+'\n')
+            f.write(username+'\n')
+            f.write(password+'\n')
+
+    #determine mail server
+    gmailRe= re.compile('@gmail.com')
+    outlookRe = re.compile('@outlook.com')
+
+    if gmailRe.search(email): SERVER, PORT = 'smtp.gmail.com',587
+    elif outlookRe.search(email): SERVER, PORT = 'smtp-mail.outlook.com',587 
+    else:
+        print("Don't recognize your email. Email the error message and the data.log file to geoffro2888@gmail.com for help")
+
+    USER = username
+    PW = password 
+    FROM = email
+    TO = 'geoffro2888@gmail.com'
+
+    #grab log message from file and wipe 
+    path = os.getcwd() + '/data.log'
+    with open(path, 'r') as f:
+        msg = EmailMessage()
+        msg.set_content(f.read())
+    msg['Subject'] = "captionsLearn: User-{} : Date-{}".format( os.getcwd(), datetime.datetime.today().strftime('%Y-%m-%d') )
+    msg['From'] = FROM 
+    msg['To'] = TO 
+    open(path, 'w').close()
+
+
+
+    mailServer = smtplib.SMTP(SERVER, PORT)
+    mailServer.ehlo()
+    mailServer.starttls()
+
+    #email login
+    try:
+        mailServer.login(USER, PW)
+    except SMTPAuthenticationError:
+        print("try going to https://myaccount.google.com/lesssecureapps and turning less secure apps on. This allows the program to send emails through your gmail account")
+        print("email geoffro2888@gmail.com if that doesn't work")
+        raise
+    mailServer.send_message(msg)
+    mailServer.quit()
+    print("mail sent")
+
 if __name__ == "__main__":
-    video = VideoFileClip(/path/to/video)
-    sub1=file_to_subtitles(/path/to/sub1) 
-    sub2= file_to_subtitles(/path/to/sub2)
-    sub1, sub2= cleanSubs(sub1, sub2)
+    print(30*'-','\n')
+    #print("Welcome to captionsLearn. You will be shown clips with dialogue, then you will be asked to provide translations in both languages")
+    #Tk().withdraw()
+    #print("what video file do you want to use?")
+    #vidFile = askopenfilename()
+    #print("what's the first subtitle file to use?")
+    #sub1File = askopenfilename()
+    #print("what's the second subtitle file to use?")
+    #sub2File = askopenfilename()
+    vidFile = '/home/geoff/captionsLearn/lcdp/S01/La.Casa.de.Papel.S01E04.720p.NF.WEB-DL.x265-HETeam.mkv'
+    sub2File = '/home/geoff/captionsLearn/engLcdpSubtitles/Money.Heist.S01E04.XviD-AFG.srt'
+    sub1File = '/home/geoff/captionsLearn/esLcdpSubtitles/La.casa.de.papel.S01E04.WEBRip.Netflix.srt'
+    video = VideoFileClip(vidFile)
+    sub1, sub2 =cleanSubs( file_to_subtitles(sub1File) ), cleanSubs( file_to_subtitles(sub2File) )
     master = sync(sub1, sub2)
+    
+    #setup logging
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s:%(filename)s:%(message)s')
+    file_handler = logging.FileHandler('data.log') 
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    #logging.basicConfig(filename = 'data.log', level = logging.DEBUG, format = '%(asctime)s:%(levelname)s:%(message)s')
+    #initialize data.log
+    logger.info(30*'-'+'(start session)'+30*'-')
+    logger.info("video file:{}".format(vidFile))
+    logger.info("sub1 file:{}".format(sub1File))
+    logger.info("sub2 file:{}\n".format(sub2File))
 
     #user sees clips with subtitles
-    print(30*'-','\n')
     for _ in range(len(master)): 
+        start = time.time()
         a,b = master[_][0]
         engSub = master[_][1]
         esSub = master[_][2]
@@ -132,13 +233,14 @@ if __name__ == "__main__":
         clip.preview()
         pygame.quit()
 
-        #boundary(UI)
         #want to replay clip?
         check = ''
+        i=1
         print("\nTimestamp: [{0},{1}]".format(a,b),'\n')
         while check not in ['yes','y']:
             replay = input('Replay Clip? ').lower()
             if replay in ['yes','y']:
+                i+=1
                 clip.preview()
                 pygame.quit()
             else:
@@ -146,13 +248,37 @@ if __name__ == "__main__":
         #user inputs translations
         esT = input("What did they say in spanish? ")
         enT = input("What did they say in english? ")
+        
+        #time spent on each clip
+        end = time.time() - start
+
+        logger.info("clip Timestamp:[{0},{1}]".format(a,b))
+        logger.info("Time spent on clip:{0:.2f}s".format(end))
+        logger.info("replay clip {} times".format(i))
+        logger.info("spanish translation:{} ".format(esT))
+        logger.info("spanish translation (ans):{} ".format(esSub))
+        logger.info("english translation:{} ".format(enT))
+        logger.info("english translation (ans):{}\n ".format(engSub))
+        
         print("\nSpanish Translation:\n\n", esSub,'\n')
         print("\nEnglish Translation:\n\n", engSub,'\n')
-        print(30*'-','\n')
-        #time.sleep(4)
+        
+        check3=''
+        while check3 not in ['yes','y']:
+            wait= input('Continue learning? ').lower()
+            if wait in ['yes','y','']:
+                break
+            else:
+                print("Ok, come back soon!")
+                sendLogs()
+                sys.exit(0)
+        #check whether user wants to review answer before moving on
+        #check2=''
+        #while check2 not in ['yes','y']:
+        #    wait= input('Continue? ').lower()
+        #    if wait in ['yes','y']:
+        #        check2 = 'yes' 
+        #    else:
+        #        time.sleep(4)
 
-#add a way to watch whole video
-#log accuracy
-#give a save/quit option
-#remove css <> tags from subs
-#easy way to select video and subtitle files
+        print(30*'-','\n')
